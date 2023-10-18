1099383b97556bab9f77a0d77c1a586d23eba33b
Ignacio Gonzalez <ignagonzalez2003@gmail.com>
2023-10-18 14:47:28 -0300

    fix: avoid the search with the same value

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,12 +1,16 @@
-import { useState, useCallback, useMemo } from "react";
+import { useState, useCallback, useMemo, useRef } from "react";
 import { getMovies } from "../service/movies";
 export function useMovies(search, sort) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const previusSearch = useRef(search);
 
   const getMoviesValues = useCallback(async (search) => {
+    if (previusSearch.current === search) return;
+
     try {
       setLoading(true);
+      previusSearch.current = search;
       const values = await getMovies(search);
       setMovies(values || []);
     } catch (error) {
